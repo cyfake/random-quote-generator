71c410900e9561752e31be74b3771650ae4348ba
cyfake <imcyfake@gmail.com>
2023-06-25 16:45:43 +0800

    Include styling into generated quotes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const generate = document.querySelector('button')
 generate.addEventListener('click', async function (e) {
     e.preventDefault();
     const res = await axios.get('https://api.quotable.io/random');
-    quote.innerText = res.data.content;
-    author.innerText = res.data.author;
+    quote.innerText = `"${res.data.content}"`;
+    author.innerText = `- ${res.data.author}`;
 })
