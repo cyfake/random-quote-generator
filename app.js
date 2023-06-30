ce9d5b45cc3663f5886f1762dc526c798be1f041
cyfake <imcyfake@gmail.com>
2023-06-30 17:32:49 +0800

    Add popup card for filter function

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const generate = document.querySelector('#generate-btn')
+
 generate.addEventListener('click', async function (e) {
     e.preventDefault();
     const res = await axios.get('https://api.quotable.io/quotes/random');
     quote.innerText = `"${res.data[0].content}"`;
     author.innerText = `- ${res.data[0].author}`;
 })
+
+const filter = document.querySelector('#filter-btn')
+const card = document.querySelector('.card');
+
+filter.addEventListener('click', function() {
+    card.classList.toggle('visible');
+}) 
